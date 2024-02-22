6840aae62335141833eb71ab5820fc0f97bfa55f
ant013 <ant013@mail.ru>
2024-02-22 12:12:42 +0600

    Implement contact filter in transactions tab

--- Sources/EvmKit/Models/TransactionTagQuery.swift
+++ Sources/EvmKit/Models/TransactionTagQuery.swift
@@ -2,14 +2,16 @@ public class TransactionTagQuery {
     public let type: TransactionTag.TagType?
     public let `protocol`: TransactionTag.TagProtocol?
     public let contractAddress: Address?
+    public let addressFilter: Address?
 
-    public init(type: TransactionTag.TagType? = nil, protocol: TransactionTag.TagProtocol? = nil, contractAddress: Address? = nil) {
+    public init(type: TransactionTag.TagType? = nil, protocol: TransactionTag.TagProtocol? = nil, contractAddress: Address? = nil, addressFilter: Address? = nil) {
         self.type = type
         self.protocol = `protocol`
         self.contractAddress = contractAddress
+        self.addressFilter = addressFilter
     }
 
     var isEmpty: Bool {
-        type == nil && `protocol` == nil && contractAddress == nil
+        type == nil && `protocol` == nil && contractAddress == nil && addressFilter == nil
     }
 }
