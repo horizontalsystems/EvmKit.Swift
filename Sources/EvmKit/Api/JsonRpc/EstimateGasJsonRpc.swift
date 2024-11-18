5d8dfec7457c9d39db241abec5d56b1cf8d662ca
EA <ealymbaev@gmail.com>
2024-11-18 17:00:22 +0600

    Make gasPrice optional for estimate_gas RPC

--- Sources/EvmKit/Api/JsonRpc/EstimateGasJsonRpc.swift
+++ Sources/EvmKit/Api/JsonRpc/EstimateGasJsonRpc.swift
@@ -2,7 +2,7 @@ import BigInt
 import Foundation
 
 class EstimateGasJsonRpc: IntJsonRpc {
-    init(from: Address, to: Address?, amount: BigUInt?, gasLimit: Int?, gasPrice: GasPrice, data: Data?) {
+    init(from: Address, to: Address?, amount: BigUInt?, gasLimit: Int?, gasPrice: GasPrice?, data: Data?) {
         var params: [String: Any] = [
             "from": from.hex,
         ]
@@ -10,19 +10,25 @@ class EstimateGasJsonRpc: IntJsonRpc {
         if let to {
             params["to"] = to.hex
         }
+
         if let amount {
             params["value"] = "0x" + (amount == 0 ? "0" : amount.serialize().hs.hex.hs.removeLeadingZeros())
         }
+
         if let gasLimit {
             params["gas"] = "0x" + String(gasLimit, radix: 16).hs.removeLeadingZeros()
         }
-        switch gasPrice {
-        case let .legacy(gasPrice):
-            params["gasPrice"] = "0x" + String(gasPrice, radix: 16).hs.removeLeadingZeros()
-        case let .eip1559(maxFeePerGas, maxPriorityFeePerGas):
-            params["maxFeePerGas"] = "0x" + String(maxFeePerGas, radix: 16).hs.removeLeadingZeros()
-            params["maxPriorityFeePerGas"] = "0x" + String(maxPriorityFeePerGas, radix: 16).hs.removeLeadingZeros()
+
+        if let gasPrice {
+            switch gasPrice {
+            case let .legacy(gasPrice):
+                params["gasPrice"] = "0x" + String(gasPrice, radix: 16).hs.removeLeadingZeros()
+            case let .eip1559(maxFeePerGas, maxPriorityFeePerGas):
+                params["maxFeePerGas"] = "0x" + String(maxFeePerGas, radix: 16).hs.removeLeadingZeros()
+                params["maxPriorityFeePerGas"] = "0x" + String(maxPriorityFeePerGas, radix: 16).hs.removeLeadingZeros()
+            }
         }
+
         if let data {
             params["data"] = data.hs.hexString
         }
